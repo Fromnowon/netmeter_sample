cd59748dcd24d1368c955114f16e0ab238bf0585
like <zihangzhao@gmail.com>
2015-10-10 16:53:20 +0800

    update
    诈尸

--- netmeter_sample/app/src/main/java/com/netmeter/like/netmeter/Fragmments/Fragment_Index.java
+++ netmeter_sample/app/src/main/java/com/netmeter/like/netmeter/Fragmments/Fragment_Index.java
@@ -1,38 +1,20 @@
 package com.netmeter.like.netmeter.Fragmments;
 
 import android.app.Fragment;
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.content.LocalBroadcastManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
-
-import com.gc.materialdesign.views.ButtonFlat;
-import com.gc.materialdesign.views.ButtonRectangle;
-import com.gc.materialdesign.views.ProgressBarCircularIndeterminate;
-import com.netmeter.like.netmeter.DataBase.DataUsageDB;
 import com.netmeter.like.netmeter.R;
-import com.rey.material.widget.Button;
-
 /**
  * Created by like on 15/6/28.
  */
 public class Fragment_Index extends Fragment {
-
-    private ButtonFlat test;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment0, container, false);
     }
-
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
